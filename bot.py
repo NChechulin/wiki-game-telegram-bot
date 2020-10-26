82c5dd570c733a0d5d347213d9785533f0bbac57
Nikolay Chechulin <nchechulin@protonmail.com>
2020-10-26 18:54:59 +0300

    add sending the path to hitler article

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import config
+from core import search
 from aiogram import Bot, Dispatcher, executor, types
 
 bot = Bot(token=config.BOT_TOKEN)
@@ -9,6 +10,13 @@ bot_dispatcher = Dispatcher(bot)
 async def welcome_handler(message: types.Message):
     '''Sends welcome message on /start command'''
     await message.reply(config.WELCOME_MESSAGE)
+
+@bot_dispatcher.message_handler()
+async def search_handler(message: types.Message):
+    try:
+        await message.reply(search.search(message.text))
+    except:
+        await message.reply('something went wrong.')
 
 
 if __name__ == "__main__":
