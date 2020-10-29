4c1b46b49dabd95d722f5a4af29fa719f5041d9c
Nikolay Chechulin <nchechulin@protonmail.com>
2020-10-29 15:30:35 +0300

    fix connection error and add add method

--- core/cache.py
+++ core/cache.py
@@ -1,11 +1,20 @@
 from core.node import Node
 import sqlite3
+from typing import List
 
 
 class Cache:
-    DB_FILE = '../cache.db'
+    DB_FILE = 'cache.db'
     cursor = None
+    conn = None
 
     def __init__(self):
-        connection = sqlite3.connect(self.DB_FILE)
-        self.cursor = connection.cursor()
+        self.conn = sqlite3.connect(self.DB_FILE)
+        self.cursor = self.conn.cursor()
+
+    def add(self, path: List[str]):
+        for i in range(len(path) - 1):
+            self.cursor.execute(
+                f"INSERT INTO cache VALUES('{path[i]}', '{path[i + 1]}')"
+            )
+        self.conn.commit()
